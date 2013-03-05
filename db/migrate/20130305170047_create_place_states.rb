b237822500fd9640c9ab1c532f9f227522bd0c04
agatov <agatovs@gmail.com>
2013-03-05 21:01:38 +0400

    place_state model state_cd column default value set to 0

--- db/migrate/20130305170047_create_place_states.rb
+++ db/migrate/20130305170047_create_place_states.rb
@@ -3,7 +3,7 @@ class CreatePlaceStates < ActiveRecord::Migration
     create_table :place_states do |t|
       t.integer :place_id
       t.integer :user_id
-      t.integer :state_cd
+      t.integer :state_cd, default: 0
 
       t.timestamps
     end
